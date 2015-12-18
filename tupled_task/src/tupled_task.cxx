533f642f59b1b5d809b65abf7c490bcfcfa6e1d6
rpeschke <richard.peschke@desy.de>
2015-12-18 14:41:49 +0100

    works now with argument forwarding

--- tupled_task/src/tupled_task.cxx
+++ tupled_task/src/tupled_task.cxx
@@ -5,15 +5,21 @@
 #include <tuple>
 #include <iostream>
 #include <memory>
+#include <vector>
 using namespace std;
 #define DEFINE_TASK(x) int __is_task(x&)
 #define  TASK_DEINITION(x) template<std::size_t N,typename T> void runTask(x, T&& next)
+#define  Template_TASK_DEINITION(x) template<std::size_t N,typename Tuple,typename T> void runTask(x, Tuple&& next)
 #define  RUN_NEXT(buffer)  runTask<N+1>(set_buffer(get<N+1>(next),buffer), next)
-#define  RUN_NEXT0(buffer)  runTask<N+1>(get<N+1>(next), next)
+#define  RUN_NEXT0()  runTask<N+1>(get<N+1>(next), next)
 
 
 template <typename T,typename... Args>
 T __check(T&& t,Args&&... );
+
+template <typename T, typename... Args>
+T __check1( Args&&...);
+
 
 template <typename T>
 auto get_buffer(T& t) {
@@ -21,20 +27,42 @@ auto get_buffer(T& t) {
 }
 
 template <typename Task_t,typename buffer_t>
-Task_t&  set_buffer(Task_t& task_,buffer_t& buffer_) {
+auto  set_buffer(Task_t& task_,buffer_t& buffer_) -> decltype (__check1<Task_t&>(task_.buffer = &buffer_)){
   task_.buffer = &buffer_;
   return task_;
 }
 class taskA {
 public:
-  int* buffer = nullptr;
-  int out_buffer = 0;
+
 };
 DEFINE_TASK(taskA);
 
+template <typename T>
+class TaskA_imple{
+public:
+  TaskA_imple(T* buffer_) :buffer(buffer_) {}
+  T* buffer ;
+
+};
+
+template <typename T> 
+TaskA_imple<T> set_buffer(taskA& task_, T& buffer_) {
+  return TaskA_imple<T>(&buffer_);
+}
+
+
+Template_TASK_DEINITION(TaskA_imple<T>& t) {
+  cout << "task A imple" << N<<endl;
+  for (auto& e: *t.buffer)
+  {
+    //set_buffer(get<0>(next), e);
+    RUN_NEXT(e);
+    //runTask<N + 1>(set_buffer(get<N + 1>(next), e), next);
+  }
+}
 TASK_DEINITION(taskA& t) {
-  cout << "task A" << endl;
-  for (int i = 0; i < 100;++i)
+ // cout << "task A" << endl;
+  for (int i = 0; i < *t.buffer;++i)
   {
     RUN_NEXT(i);
   }
@@ -49,7 +77,8 @@ DEFINE_TASK(taskB);
 
 TASK_DEINITION(taskB& t) {
   cout << "task B" << *t.buffer<< endl;
-  RUN_NEXT0();
+ // get<N >(next);
+  RUN_NEXT(*t.buffer);
 }
 
 
@@ -57,8 +86,13 @@ class stop {
   
 };
 DEFINE_TASK(stop);
-template<std::size_t N, typename T>
-void runTask(stop& t, T&& next) {
+
+template<typename T>
+stop& set_buffer(stop& s, T&&) {
+  return s;
+}
+
+TASK_DEINITION(stop&){
   cout << "stop" << endl;
  
 }
@@ -69,7 +103,8 @@ public:
   batch(T& t) :m_tuple(t) {}
   T m_tuple;
 };
-
+template<typename T>
+int __is_batch(batch<T>&);
 template<typename T>
 batch<T> make_batch(T&& t) {
   return batch<T>(t);
@@ -107,24 +142,32 @@ void runTask(T&& t) {
 }
 
 template<typename T>
-void runTask(batch<T> tup) {
+void runTask(batch<T>& tup) {
   auto t1 = tup >> stop();
   runTask(t1.m_tuple);
  // runTask<0>(get<0>(t1.m_tuple), t1.m_tuple);
 
 }
 
+template <typename T, typename T1>
+auto operator|(T&& t, batch<T1> && batch_) {
+  auto task_ = batch_ >> stop();
 
+ auto ta= set_buffer(get<0>(task_.m_tuple), t);
+  runTask<0>(ta, task_.m_tuple);
+  
+}
 int main()
 {
 
   auto t = make_tuple(taskA(), taskB());
   auto t2 = tuple_cat(t, t ,make_tuple(stop()));
   
+  std::vector<int> vec{ 1,2,344,5,65,435,35,5,345 };
  // set_buffer(taskB(), get_buffer(taskA()));
-  auto b = taskA() >> taskB();
+  vec | taskA() >> taskB()>>taskB();
   // auto t1 = tuple_cat(t, stop());
-  runTask(b);
+  //runTask(b);
  // runTask(t2);
     return 0;
 }
